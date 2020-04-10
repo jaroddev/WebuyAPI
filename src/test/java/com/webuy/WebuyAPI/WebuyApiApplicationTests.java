e4d4608c3e2e2f2354fcb8ef56012e2e0e410ec5
sitedevente <martinjarod17@gmail.com>
2020-04-10 20:36:19 +0200

    Initialzed Spring API project for the WeBuy android app

---
+++ src/test/java/com/webuy/WebuyAPI/WebuyApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.webuy.WebuyAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebuyApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
