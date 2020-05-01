09c1f4e5843ab74937559b449fc71a578dbd1af8
sitedevente <martinjarod17@gmail.com>
2020-05-01 17:50:06 +0200

    Generated getters and setters for User class

--- src/main/java/com/webuy/WebuyAPI/entities/User.java
+++ src/main/java/com/webuy/WebuyAPI/entities/User.java
@@ -5,6 +5,9 @@ import java.util.Collection;
 
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToOne;
 import javax.persistence.GeneratedValue;
 
 @Entity
@@ -16,8 +19,67 @@ public class User implements Serializable {
 	private String phoneNumber;
 	private String password;
 	private String role;
+
+	@OneToOne
 	private Address address;
+	
+	@ManyToMany
 	private Collection<User> friends;
-	
-	
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPhoneNumber() {
+		return phoneNumber;
+	}
+
+	public void setPhoneNumber(String phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getRole() {
+		return role;
+	}
+
+	public void setRole(String role) {
+		this.role = role;
+	}
+
+	public Address getAddress() {
+		return address;
+	}
+
+	public void setAddress(Address address) {
+		this.address = address;
+	}
+
+	public Collection<User> getFriends() {
+		return friends;
+	}
+
+	public void setFriends(Collection<User> friends) {
+		this.friends = friends;
+	}
+
 }
