e6c26b654804ee357eeb85c199f23f985afdacf8
sitedevente <martinjarod17@gmail.com>
2020-05-08 16:41:58 +0200

    Added Shop service as autowired to controller

--- src/main/java/com/webuy/WebuyAPI/web/ShopController.java
+++ src/main/java/com/webuy/WebuyAPI/web/ShopController.java
@@ -1,42 +1,37 @@
 package com.webuy.WebuyAPI.web;
 
-import java.util.List;
+import java.util.Collection;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.webuy.WebuyAPI.dao.ShopJPARepository;
 import com.webuy.WebuyAPI.entities.Shop;
+import com.webuy.WebuyAPI.services.ShopServiceImpl;
 
 @RestController
 public class ShopController {
 	
 	@Autowired
-	private ShopJPARepository shopRepo;
+	private ShopServiceImpl shopService;
 
-	
 	public ShopController() {}
 	
-	/**
-	 * shop or shops
-	 * */
 	@GetMapping("/shop")
-	public List<Shop> getShopList(){
-		return this.shopRepo.findAll();
+	public Collection<Shop> getShopList(){
+		return this.shopService.getShops();
 	}
 	
 	@GetMapping("/shop/{shopId}/groups")
-	public boolean getGroupList(@PathVariable(value="shopID") Long id) {
-		// use the shop Repository
-		// true return type is List<Group>
+	public boolean getGroupList(@PathVariable Long id) {
+		//this.shopService.getOne(id).get
 		
 		return true;
 	}	
 	
 	@GetMapping("/shop/{shopId}/products")
-	public boolean getProductList(@PathVariable(value="shopID") Long id) {
+	public boolean getProductList(@PathVariable Long id) {
 		// use the shop Repository
 		// true return type is List<Product>
 		
