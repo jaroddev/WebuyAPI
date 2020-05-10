b91bb6c46d7f4e37b3ec812ceb8bf01a00f6300f
sitedevente <martinjarod17@gmail.com>
2020-05-10 21:48:25 +0200

    Added Shop crud routes to rest controller

--- src/main/java/com/webuy/WebuyAPI/web/ShopController.java
+++ src/main/java/com/webuy/WebuyAPI/web/ShopController.java
@@ -3,8 +3,12 @@ package com.webuy.WebuyAPI.web;
 import java.util.Collection;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.webuy.WebuyAPI.entities.Shop;
@@ -23,6 +27,21 @@ public class ShopController {
 		return this.shopService.getShops();
 	}
 	
+	@PostMapping("/shop")
+	public void addShop(@RequestBody Shop shop) {
+		this.shopService.createShop(shop);
+	}
+	
+	@PutMapping("/shop")
+	public void editShop(@PathVariable Long id, @RequestBody Shop shop) {
+		this.shopService.updateShop(id, shop);
+	}
+	
+	@DeleteMapping("/shop")
+	public void deleteShop(@PathVariable Long id) {
+		this.shopService.deleteShop(id);
+	}
+	
 	@GetMapping("/shop/{shopId}/groups")
 	public boolean getGroupList(@PathVariable Long id) {
 		//this.shopService.getOne(id).get
@@ -39,5 +58,3 @@ public class ShopController {
 	}
 	
 }
-
-
