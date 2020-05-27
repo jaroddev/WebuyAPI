68b37dad531bfeec96742c1f9e06c16c4f0e737a
Arondefao <doglobegerard@gmail.com>
2020-05-27 19:05:16 +0200

    creation of static folders for images

--- src/main/java/com/webuy/WebuyAPI/web/ImageController.java
+++ src/main/java/com/webuy/WebuyAPI/web/ImageController.java
@@ -1,15 +1,21 @@
 package com.webuy.WebuyAPI.web;
 
+import java.io.InputStream;
 import java.util.Collection;
 import java.util.Optional;
 
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.webuy.WebuyAPI.dao.ImageJPARepository;
@@ -21,6 +27,10 @@ public class ImageController {
 	@Autowired
 	private ImageJPARepository imageJpaRepository;
 
+	
+	
+	    
+	
 	@GetMapping("/images")
 	public Collection<Image> getAllImages() {
 		return this.imageJpaRepository.findAll();
